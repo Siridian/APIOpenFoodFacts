56fc4e3f934ec908fcf08ace65a4794cb6354f76
Thierry Chappuis <tchappui@users.noreply.github.com>
2019-05-27 09:43:38 +0200

    Commented update_db.py

--- update_db.py
+++ update_db.py
@@ -8,6 +8,7 @@ from local_settings import db_config
 
 import requests
 import json
+# -tc- Si tu utilises records, l'import de mysql.connector est inutile. Records l'utilise en interne
 import mysql.connector
 import records
 
@@ -16,15 +17,32 @@ accepted_categories = ["Boissons", "Plats préparés", "Produits laitiers", "Sna
 
 
 def update_db():
+    # -tc- de manière plus lisible, j'écrirais:
+    # params = {
+    #     "action": "process",
+    #     "tagtype_0": "categories",
+    #     "tag_contains_0": "contains",
+    #     "tag_0": category,
+    #     "json": 1,
+    #     "page_size": 1000
+    # }
+    # r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=params)
+    #
+    # Pourquoi se contenter de une seule catégorie et de un seul nutriscore? Nous ne sommes pas obligé d'avoir que des A
     r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl?search_terms=pizza&nutrition_grades=a&search_simple=1&json=1')
     products = r.json()["products"]
     db = records.Database(os.environ.get(DB_CREDENTIALS))
     
+    
+    # -tc- Personnellement, je séparerais interaction avec l'API avec gestion des produits en base dans des classes séparées
 
+    # -tc- Pour écrire des requêtes sur plusieurs lignes, on peut utiliser des triples guillemets ou triples apostrophes
     add_product = ('INSERT INTO Products'
                    '(name, grade, link, description)'
+                   # -tc- records utilisera 'VALUES (:product_name,  :nutrition_grades, :url, :generic_name)'
                    'VALUES (%(product_name)s,  %(nutrition_grades)s, %(url)s, %(generic_name)s)')
 
+    # -tc- INSERT IGNORE fonctionne avec records
     add_store = ('INSERT INTO Stores'
                   '(name)'
                   'VALUES (%(name)s)'
@@ -52,6 +70,17 @@ def update_db():
             "url": product["url"],
             "generic_name": product["generic_name"]
             }
+            # -tc- Ne jamais utiliser de format pour faire de l'insertion, risques d'injections sql, 
+            # -tc- Records fournit un mécanisme pour l'éviter
+            # db.query(
+            #     """INSERT INTO Products (name, grade, link, description) 
+            #        VALUES (:product_name, :nutrition_grades, :url, :description
+            #     """, 
+            #     product_name=product["product_name"], 
+            #     nutrition_grades=product["nutrition_grades"], 
+            #     product["url"], 
+            #     description=product["generic_name"])
+            #  )
             db.query('INSERT INTO Products (name, grade, link, description) VALUES (\'{}\', \'{}\', \'{}\', \'{}\')'.format(product["product_name"], product["nutrition_grades"], product["url"], product["generic_name"]))
             
             
@@ -59,13 +88,16 @@ def update_db():
 
             
             for store in stores:
+                # -tc- Idem que plus haut, éviter format
                 db.query('INSERT INTO Stores (name) VALUES (\'{}\') ON DUPLICATE KEY UPDATE name = \'{}\''.format(store, store))
                 
                 store_id = (db.query('SELECT LAST_INSERT_ID()')[0]["LAST_INSERT_ID()"])
+                # -tc- Idem que plus haut, éviter format
                 db.query('INSERT INTO Product_Stores VALUES ({}, {})'.format(product_id, store_id))
             
             
             for category in categories:
+                # -tc- Idem que plus haut, éviter format
                 category_id = db.query('SELECT id FROM Categories WHERE name = \'{}\''.format(category))[0]["id"]
                 db.query('INSERT INTO Product_Categories VALUES ({}, {})'.format(product_id, category_id))
 
@@ -76,7 +108,7 @@ def main():
     update_db()
 
 
-
+    
 
 if __name__ == "__main__":
     main()
